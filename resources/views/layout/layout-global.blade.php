dd45178bc01b6ef889c147850139fc7bf1a8157f
leandroricke100 <leandro_henrique_100@hotmail.com>
2024-01-25 08:37:18 -0300

    alteração da font

--- resources/views/layout/layout-global.blade.php
+++ resources/views/layout/layout-global.blade.php
@@ -10,8 +10,12 @@
 
     <link rel="stylesheet" href="{{ asset('fonts/fontawesome/fontawesome-pro.css') }}" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link
+    {{-- <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"> --}}
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
         rel="stylesheet">
 
     <script src="{{ asset('js/tools/jquery.min.js') }}"></script>
--- resources/views/pages/page-atendimento.blade.php
+++ resources/views/pages/page-atendimento.blade.php
@@ -37,8 +37,32 @@
                         <span>Situação atual: </span>
                         <p>Arquivado |</p>
                         <span>Código nº:</span>
-                        <p>593.617.060.959.202.054</p>
+                        <p>593.617.060</p>
                     </div>
+                </div>
+            </div>
+
+            <div class="container-avalicao">
+                <div class="avalicao">
+                    <p>AVALIE ATENDIMENTO</p>
+                    <p>Pesquisa de Satisfação</p>
+                </div>
+
+                <div class="rating">
+                    <input value="5" name="rate" id="star5" type="radio">
+                    <label title="text" for="star5"></label>
+
+                    <input value="4" name="rate" id="star4" type="radio">
+                    <label title="text" for="star5"></label>
+
+                    <input value="3" name="rate" id="star3" type="radio">
+                    <label title="text" for="star5"></label>
+
+                    <input value="2" name="rate" id="star2" type="radio">
+                    <label title="text" for="star5"></label>
+
+                    <input value="1" name="rate" id="star1" type="radio">
+                    <label title="text" for="star5"></label>
                 </div>
             </div>
         </section>
