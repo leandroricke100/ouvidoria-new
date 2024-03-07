1ce09162a0ec87299fcde82c9e9217533fd8929a
leandro henrique <leandro_henrique_100@hotmail.com>
2024-03-07 17:22:44 -0300

    atualizando o arquivo page-inicio.css e page-inicio.blade.php

--- resources/views/pages/page-inicio.blade.php
+++ resources/views/pages/page-inicio.blade.php
@@ -56,30 +56,6 @@
 
         <div class="container">
             <div class="container-left">
-                {{-- <h3>Serviços</h3>
-            @if (count($menus) === 0)
-                <div class="menus">
-                    <div class="icons"><span><i class="fas fa-arrow-right"></i></span></div>
-                    <div class="opcoes">
-                        <h2>Nenhum menu cadastrado</h2>
-                    </div>
-                </div>
-            @else
-                @foreach ($menus as $menu)
-                    @if ($menu->status == 1)
-                        <div class="menus">
-                            <div class="icons"><span><i class="fas fa-arrow-right"></i></span></div>
-                            <div class="opcoes">
-                                <a href="{{ $menu->slog }}">{{ $menu->titulo }}</a>
-                            </div>
-                        </div>
-                    @endif
-                @endforeach
-            @endif --}}
-
-
-                {{-- @include('components.comp-login', ['id' => 'login-home']) --}}
-
 
                 <div class="opcoes-home">
                     <span class="titulo-bx"><i class="fas fa-caret-right"></i> Selecione uma opção abaixo</span>
@@ -130,12 +106,12 @@
 
                 @endif
 
+                <div class="transparencia">
+                    <p class="prefeitura"><i class="fas fa-fw fa-chart-bar"></i> Transparência</p>
 
-                <a href="/transparencia" class="titulo-bx"><i class="fas fa-fw fa-chart-bar"></i> Transparência</a>
 
-                {{-- <div class="menus-right">
-                <button><i class="far fa-thumbs-up"></i> <strong>Consulta Prévia Online</strong></button>
-            </div> --}}
+                    <a href="/transparencia" class="transparencia-btn"  >Ver Transparencia </a>
+                </div>
 
                 <div class="info-prefeitura">
                     <p class="prefeitura">{{ $OuvidoriaConfiguracao->first()->titulo }}</p>
@@ -143,21 +119,9 @@
                         <p>{!! $OuvidoriaConfiguracao->first()->informacoes !!}</p>
                     </div>
 
-                    <button class="organograma">Ver Organograma <i class="fal fa-angle-double-right"></i></button>
                 </div>
 
-                {{-- <div class="ajuda">
-                <p>Precisa de Ajuda?</p>
-                <div class="links-ajuda">
-                    <a href="">Como criar um arquivo de projeto/prancha no formato PDF padrão 1Doc</a>
-                    <a href="">Utilizando o Login Único gov.br como acesso na Central de Atendimento 1Doc</a>
-                    <a href="">Como criar uma solicitação na Central de Atendimento da prefeitura</a>
-                    <a href="">Como acompanhar o andamento da sua solicitação</a>
-                    <a href="">Como pesquisar documentos, interagir e anexar novas informações.</a>
-                    <a href="">Como realizar cadastro na central de atendimento da entidade</a>
-                    <a href="">Recuperando seu acesso na central de atendimento da Plataforma 1Doc</a>
-                </div>
-            </div> --}}
+
             </div>
         </div>
 
